fffda481f74d84fd220e0563e2ef37793fa090f0
Saksham Checker <checkersaksham@yahoo.com>
2024-03-04 16:00:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def train_a_model(target_model, dataset, target_indices, attack_test_indices, nu
     target_train_loader = DataLoader(target_train_dataset, batch_size=batch_size)
     target_test_loader = DenseDataLoader(target_test_dataset, batch_size=batch_size)
     if coarsen:
-        target_train_loader=coarsen_a_data(cus_dataloader=target_train_loader, coarsen_params=[0.01, 0.01, 0.01, 0.01], batch_size=batch_size, max_nodes=max_nodes)
+        target_train_loader=coarsen_a_data(cus_dataloader=target_train_loader, coarsen_params=[0.01, 0.01, 0.01, 0.01], batch_size=batch_size)
     filtered_train_loader=[]
     denser=ToDense(max_nodes)
     filter=MyFilter(max_nodes)
